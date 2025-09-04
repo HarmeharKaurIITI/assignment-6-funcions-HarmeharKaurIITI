999efff139ff7593eaf7abc6ec2cb3838712d900
HarmeharKaurIITI <me250003034@iiti.ac.in>
2025-09-04 11:43:06 +0530

    Update second_largest.cpp

--- second_largest.cpp
+++ second_largest.cpp
@@ -5,7 +5,20 @@ using namespace std;
 
 int secondLargest(const vector<int>& arr) {
     // TODO: complete the function as per instructions
-
+if (arr.size()<2) {
+    return -1;}
+int largest= INT_MIN;
+int second= INT_MIN;
+for (int j=0;j<arr.size();j++){
+     if (arr[j]>largest) {
+         second=largest;
+         largest=arr[j];}
+     else if (arr[j]>second && arr[j]!=largest){
+         second=arr[j];}
+}
+if (second==INT_MIN) {
+return -1;}
+return second;
 }
 
 int main() {
@@ -16,4 +29,3 @@ int main() {
     cout << secondLargest(arr) << "\n";
     return 0;
 }
-
